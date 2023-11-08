52aa3415bca1fe6802e4dd148cc9214bd9fe44fb
hao wei <1120575939@qq.com>
2023-11-09 07:36:49 +0800

    新增MoveZeroes.js

---
+++ MoveZeroes.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+
+};
+
+// 输入: nums = [0,1,0,3,12]
+// 输出: [1,3,12,0,0]
+
+let nums = [0, 1, 0, 3, 12]
+
