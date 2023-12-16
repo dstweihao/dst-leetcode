8190305893dac9f2b8ba02b7f5da92faf99a37db
ZLGMCU\weihao <weihao@zlg.cn>
2023-12-16 09:17:46 +0800

    no message

---
+++ 14.LongestCommonPrefix.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+
+let strs = ["flower", "flow", "flight"]
+
+var longestCommonPrefix = function (strs) {
+
+};
+
+longestCommonPrefix(strs)
---
+++ test.js
@@ -0,0 +1,5 @@
+console.time('test')
+// console.time('test')
+console.timeEnd('test')
+
+
