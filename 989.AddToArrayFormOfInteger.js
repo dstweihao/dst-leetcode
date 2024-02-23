63f096d076aa9005aec12002b51e822d3e7a7e59
weihao <weihao@zlg.cn>
2024-02-23 15:28:05 +0800

    no message

---
+++ 989.AddToArrayFormOfInteger.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} num
+ * @param {number} k
+ * @return {number[]}
+ */
+
+var num = [1,2,0,0]
+var k = 34
+var out = [1,2,3,4]
+var addToArrayForm = function(num, k) {
+
+};
