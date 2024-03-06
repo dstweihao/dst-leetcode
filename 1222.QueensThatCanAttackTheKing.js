cfe88d88dcb4499238f7c4c1a2e6384efe9b6f59
1120575939@qq.com <weihao@zlg.cn>
2024-03-06 10:24:10 +0800

    no message

--- 1222.QueensThatCanAttackTheKing.js
+++ 1222.QueensThatCanAttackTheKing.js
@@ -3,6 +3,22 @@
  * @param {number[]} king
  * @return {number[][]}
  */
-var queensAttacktheKing = function(queens, king) {
+const queens = [[0, 1], [1, 0], [4, 0], [0, 4], [3, 3], [2, 4]], king = [0, 0]
+const out = [[0, 1], [1, 0], [3, 3]]
+const queensAttacktheKing = function (queens, king) {
+    let result = []
+    for (const element of queens) {
+        // 判断国王四周，如果x或者y相减为0，意味着与国王位置在一条直线上
+        if (Math.abs(element[0] - king[0]) == 0 || Math.abs(element[1] - king[1]) == 0) {
+            result.push(element)
+        }
+        // 判断对角线
+        if (Math.abs(element[0] - king[0]) == Math.abs(element[1] - king[1])) {
+            result.push(element)
+        }
 
+    }
+    return result
 };
+
+console.log(queensAttacktheKing(queens, king))
