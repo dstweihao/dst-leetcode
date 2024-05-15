d6ba6fb5570b156e76c1598b190da1b3d9fc121b
hao wei <1120575939@qq.com>
2024-05-16 00:20:30 +0800

    no message

---
+++ 498.DiagonalTraverse.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[][]} mat
+ * @return {number[]}
+ */
+
+
+const mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+const output = [1, 2, 4, 7, 5, 3, 6, 8, 9]
+
+const findDiagonalOrder = function (mat) {
+    
+    return mat
+};
+
+console.log(findDiagonalOrder(mat))
