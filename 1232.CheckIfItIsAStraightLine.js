b4202671d242dc3c4075dd362c0d490a06cc6e29
weihao <weihao@zlg.cn>
2024-08-09 14:30:53 +0800

    no message

--- 1232.CheckIfItIsAStraightLine.js
+++ 1232.CheckIfItIsAStraightLine.js
@@ -3,99 +3,40 @@
  * @return {boolean}
  */
 const coordinates = [
-  [0, 0],
-  [0, 1],
-  [0, -1],
+  [1, 2],
+  [2, 3],
+  [3, 4],
+  [4, 5],
+  [5, 6],
+  [6, 7],
 ];
-// [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
-// const coordinates = [
-//   [0, 1],
-//   [1, 3],
-//   [-4, -7],
-//   [5, 11],
-// ];
-// let checkStraightLine = function (coordinates) {
 
-//     const allXEqual = coordinates.every((item) => item[0] === coordinates[0][0]
-//     );
-//     if (allXEqual) {
-//       return true;
-//     }
-
-//     const allYEqual = coordinates.every((item) => item[1] === coordinates[0][1]
-//     );
-//     if (allYEqual) {
-//       return true;
-//     }
-//   // 两点确定一条直线
-//   // 斜率相同
-//   const slope =(coordinates[1][1] - coordinates[0][1]) / (coordinates[1][0] - coordinates[0][0]);
-
-//   for (let i = 0; i < coordinates.length; i++) {
-//     if (i === coordinates.length - 1) {
-//       break;
-//     }
-//     let yDifference = coordinates[i + 1][1] - coordinates[i][1];
-//     let xDifference = coordinates[i + 1][0] - coordinates[i][0];
-//     if (yDifference / xDifference !== slope) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
-let checkStraightLine = function (coordinates) {
+const checkStraightLine = function (coordinates) {
   // 两点确定一条直线
-  if (coordinates.length !== 2) {
+  if (coordinates.length < 2) {
     return false;
   }
-
-  const allXEqual = coordinates.every((item) => item[0] === coordinates[0][0]);
-  if (allXEqual) {
+  // 同一个X轴
+  const allEqualX = coordinates.every((item) => item[0] === coordinates[0][0]);
+  if (allEqualX) {
     return true;
   }
-
-  const allYEqual = coordinates.every((item) => item[1] === coordinates[0][1]);
-  if (allYEqual) {
+  // 同一个Y轴
+  const allEqualY = coordinates.every((item) => item[1] === coordinates[0][1]);
+  if (allEqualY) {
     return true;
   }
-
-  const { x1, y1 } = coordinates[0];
-  const { x2, y2 } = coordinates[1];
-  const slope = (y2 - y1) / (x2 - x1);
-
+  // 对角线
+  const [x0, y0] = coordinates[0];
+  const [x1, y1] = coordinates[1];
+  // 遍历剩余的坐标点，检查斜率是否一致
   for (let i = 2; i < coordinates.length; i++) {
-    const { x, y } = coordinates[i];
-    // 斜率相同
-    if ((y - y1) / (x - x1) !== (y2 - y1) / (x2 - x1)) {
-      return false;
-    }
-    // (x - x1)*(x2 - x1)
-  }
-
-  // 两点确定一条直线
-
-  for (let i = 0; i < coordinates.length; i++) {
-    if (i === coordinates.length - 1) {
-      break;
-    }
-    let yDifference = coordinates[i + 1][1] - coordinates[i][1];
-    let xDifference = coordinates[i + 1][0] - coordinates[i][0];
-    if (yDifference / xDifference !== slope) {
+    const [xi, yi] = coordinates[i];
+    // 避免除以零，并且使用交叉乘积来比较斜率
+    if ((yi - y0) * (x1 - x0) !== (y1 - y0) * (xi - x0)) {
       return false;
     }
   }
   return true;
 };
-console.log(checkStraightLine(coordinates));
 
-const { locale } = useI18n()
-const $t = t[locale.value]
-let popVisible = ref(false)
-let visible = ref(false)
-let timer: any = null
-let needPolling = true
-let currentCapacity = ref(0)
-const pieLoading = ref<boolean>(false)
-const echartRef = ref<HTMLElement | null>(null)
-let myChart: echarts.ECharts | null = null
-let currentCapacityStatus = ref('normal')
