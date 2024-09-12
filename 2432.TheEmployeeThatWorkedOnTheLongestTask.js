ceb6a0f86e261d483e569524b998ea52e95ade63
weihao <weihao@zlg.cn>
2024-09-12 17:41:32 +0800

    no message

--- 2432.TheEmployeeThatWorkedOnTheLongestTask.js
+++ 2432.TheEmployeeThatWorkedOnTheLongestTask.js
@@ -3,6 +3,25 @@
  * @param {number[][]} logs
  * @return {number}
  */
-let hardestWorker = function(n, logs) {
-    
+
+let n = 10, logs = [[0, 3], [2, 5], [0, 9], [1, 15]]
+let hardestWorker = function (_n, logs) {
+    let longTime = logs[0][1]
+    let difference = [[logs[0][0], logs[0][1]]]
+    logs.forEach((_log, i) => {
+        if (i >= logs.length - 1) {
+            return
+        }
+        difference.push([logs[i + 1][0], logs[i + 1][1] - logs[i][1]])
+        longTime = Math.max(longTime, logs[i + 1][1] - logs[i][1])
+    });
+    const fill = difference.filter(diff => {
+        return diff[1] == 6
+    })
+    let fiu = []
+    fill.forEach(ite=>{
+        fiu.push(ite[0])
+    })
+    return Math.min(...fiu)
 };
+hardestWorker(n, logs)
