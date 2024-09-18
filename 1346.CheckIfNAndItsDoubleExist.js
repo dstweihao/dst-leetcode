b1b74f1f5c0b9a37582542e426e36d7bbe4697c4
hao wei <1120575939@qq.com>
2024-09-18 23:44:27 +0800

    no message

--- 1346.CheckIfNAndItsDoubleExist.js
+++ 1346.CheckIfNAndItsDoubleExist.js
@@ -9,7 +9,14 @@
    arr[i] == 2 * arr[j]
  */
 
-const arr = [7,1,14,11]
-const checkIfExist = function(arr) {
-    
+// const arr = [7, 1, 14, 11]
+// const arr = [-2, 0, 10, -19, 4, 6, -8]
+const arr = [0, 0]
+
+const checkIfExist = function (arr) {
+  const result = arr.map((m, i) => {
+    const index = arr.indexOf(m * 2)
+    return index != -1 && index != i
+  })
+  return result.includes(true)
 };
