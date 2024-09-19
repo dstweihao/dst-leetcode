c9ce419ccdc07f9d32a9681ef97c44d40bcdcd7c
hao wei <1120575939@qq.com>
2024-09-19 22:46:24 +0800

    no message

--- 189.RotateArray.js
+++ 189.RotateArray.js
@@ -4,10 +4,18 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 
-const nums = [1, 2, 3, 4, 5, 6, 7]
-const k = 3
-
-let rotate = function (nums, k) {
-
-
+// const nums = [1, 2, 3, 4, 5, 6, 7]
+// const k = 3
+const nums = [1, 2]
+const k = 5
+const rotate = function (nums, k) {
+    let count = 0;
+    if (k <= nums.length) {
+        count = k;
+    } else {
+        count = k % nums.length;
+    }
+    const middlePart = nums.splice(nums.length - count, count);
+    nums.unshift(...middlePart);
+    return nums;
 }
