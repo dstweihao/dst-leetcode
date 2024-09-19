96df33fde7e853831ef3fac6e3508fcedeaa21e2
weihao <weihao@zlg.cn>
2024-09-19 11:21:16 +0800

    no message

---
+++ 189.RotateArray.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+
+const nums = [1, 2, 3, 4, 5, 6, 7]
+const k = 3
+
+let rotate = function (nums, k) {
+
+
+}
