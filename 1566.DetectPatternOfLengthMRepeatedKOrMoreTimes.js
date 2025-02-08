cc36182a849cd75151a0734bfd836aad91b1f75b
weihao@zlg.cn <weihao@zlg.cn>
2025-02-08 17:02:29 +0800

    no message

---
+++ 1566.DetectPatternOfLengthMRepeatedKOrMoreTimes.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} arr
+ * @param {number} m
+ * @param {number} k
+ * @return {boolean}
+ */
+/**
+ * 
+输入：arr = [1,2,4,4,4,4], m = 1, k = 3
+输出：true
+解释：模式 (4) 的长度为 1 ，且连续重复 4 次。注意，模式可以重复 k 次或更多次，但不能少于 k 次。
+示例 2：
+ */
+const arr = [1,2,4,4,4,4]
+const m = 1
+const k = 3
+const containsPattern = function(arr, m, k) {
+    
+};
+console.log(containsPattern([1,2,4,4,4,4], 1, 3)) // true
