5cd6766958c7dab30183c66e280f8a6e0b8f0942
weihao@zlg.cn <weihao@zlg.cn>
2025-04-21 17:40:40 +0800

    no message

--- 3280.ConvertDateToBinary.js
+++ 3280.ConvertDateToBinary.js
@@ -2,6 +2,12 @@
  * @param {string} date
  * @return {string}
  */
+/**
+输入： date = "2080-02-29"
+输出： "100000100000-10-11101"
+解释：
+100000100000, 10 和 11101 分别是 2080, 02 和 29 的二进制表示。 
+ */
 var convertDateToBinary = function(date) {
     
 };
