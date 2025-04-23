c2e6cfd5b5302efddd4a1e240588530021875893
weihao@zlg.cn <weihao@zlg.cn>
2025-04-23 10:31:15 +0800

    no message

--- 3280.ConvertDateToBinary.js
+++ 3280.ConvertDateToBinary.js
@@ -8,6 +8,11 @@
 解释：
 100000100000, 10 和 11101 分别是 2080, 02 和 29 的二进制表示。 
  */
-var convertDateToBinary = function(date) {
-    
+const convertDateToBinary = function(date) {
+    const [year, month, day] = date.split('-')
+    const yearBinary = parseInt(year).toString(2)
+    const monthBinary = parseInt(month).toString(2)
+    const dayBinary = parseInt(day).toString(2)
+    return `${yearBinary}-${monthBinary}-${dayBinary}`
 };
+console.log(convertDateToBinary('2080-02-29'));
